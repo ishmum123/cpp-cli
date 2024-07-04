e3fea03eba483571ee814aab75c7623ec2ed20a0
Shakil Ahmed <44522075+ahmedshakill@users.noreply.github.com>
2024-07-04 14:26:41 +0600

    adds validation for single flag

--- src/autoparser.h
+++ src/autoparser.h
@@ -26,14 +26,41 @@ absl::StatusOr<T> autoparse(int argc, char *argv[], std::vector<std::string> inp
     // TODO: Validation
     // Now I would like to check if T has a member of type flag
 
+    auto val_res = validate(argc,argv);
 
 
+    if(std::is_same<T, Config>::value){
+        std::cout<<"T is of type Config"<<std::endl;
 
-
+    }
 
     const auto view = rfl::to_view<T>(t);
     view.apply([](const auto&f ){
         std::cout<<f.name()<<": "<< rfl::json::write(f.value())<<std::endl;
     });
     return t;
+}
+
+bool isInteger(string str){
+    if(str.starts_with('-')){
+        str.erase(str.begin());
+    }
+    return std::all_of(str.begin(), str.end(),::isdigit);
+}
+
+absl::Status validate(int argc, char *argv[]){
+    if(strcmp("--single",argv[0])){
+        if(strcmp("true",argv[1]) || strcmp("false", argv[1]))
+            return absl::OkStatus();
+        else return absl::InvalidArgumentError(absl::StrFormat("single (boolean) - %s is not a valid input. Please see --help",argv[1]));
+    }
+    else if(strcmp("--integer", argv[0])){
+        if(isInteger(static_cast<string>(argv[1])))
+            return absl::OkStatus();
+        else return absl::InvalidArgumentError(absl::StrFormat("integer - %s is not a valid input. Please see --help",argv[1]));
+    }
+    else if(strcmp("--string",argv[0])){
+        return absl::OkStatus();
+    }      
+    else return absl::InvalidArgumentError(absl::StrFormat("%s - %s is not a valid input. Please see --help",argv[0],argv[1]));  
 }
